1df5179764aaf56173a9bdeae3cb867aed5287a3
OvO7000 <13818754283@163.com>
2019-06-03 16:29:22 +0800

    封装db.js

---
+++ models/db.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017/list'
+const config = {
+    useNewUrlParser: true
+}
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(url, config)
+    .then(db => {
+        console.log('--------------------------');
+        console.log('connect mongodb success');
+        console.log('--------------------------');
+    })
+    .catch(err =>{
+        console.log('--------------------------');
+        console.log('connect mongodb failed');
+        console.log('--------------------------');
+    })
+
+
+function model (name, option, index) {
+    // 添加默认字段
+    const allOption = {
+
+        created_at: {type: Date, default: Date.now()},
+        update_at: {type: Date, default: Date.now()},
+        deleted_at: {type: Date, default: Date.now()},
+        is_deleted: {type: Boolean, default: false},
+        ...option
+    }
+    const schema = new mongoose.Schema(option)
+    // 创建索引
+
+    if (index) {
+        schema.index(index)
+    }
+
+    const model = mongoose.model(name, schema)
+
+    /**
+     * 创建文档
+     */
+    let create = option =>{
+        const document = new model(option)
+        return document.save()
+    }
+
+    return {
+        model,
+        create
+    }
+}
+
+module.exports = model
---
+++ models/type.js
@@ -0,0 +1,13 @@
+const model = require('./db')
+
+const name = 'Type'
+const schema = {
+    subType: {
+        name: { type: String, required: true },
+        name_en: { type: String, required: true }
+    },
+    type: { type: String, required: true },
+    secret: { type: Boolean, default: false}
+}
+
+module.exports = model(name, schema)
